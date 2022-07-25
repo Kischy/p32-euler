a360e117e682a4ffcf01f16b997138daae0e1ae7
Kischy <kischy@gmx.de>
2022-07-25 22:09:31 +0200

    Added get all digits function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod pandigital_products;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/pandigital_products/digits.rs
@@ -0,0 +1,43 @@
+fn get_digits(num: u128) -> Vec<u32> {
+    num.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect()
+}
+
+pub fn get_all_digits(nums: Vec<u128>) -> Vec<u32> {
+    let mut digits = Vec::new();
+    for ele in nums {
+        digits.append(&mut get_digits(ele));
+    }
+
+    digits
+}
+
+#[cfg(test)]
+mod tests {
+    use super::get_all_digits;
+    use super::get_digits;
+
+    #[test]
+    fn different_tests_of_get_digits() {
+        assert_eq!(get_digits(0), vec![0]);
+        assert_eq!(get_digits(123), vec![1, 2, 3]);
+        assert_eq!(get_digits(5032), vec![5, 0, 3, 2]);
+        assert_eq!(get_digits(9), vec![9]);
+    }
+
+    #[test]
+    fn different_tests_of_get_all_digits() {
+        assert_eq!(get_all_digits(vec![0]), vec![0]);
+        assert_eq!(get_all_digits(vec![0, 123]), vec![0, 1, 2, 3]);
+        assert_eq!(
+            get_all_digits(vec![0, 123, 5032]),
+            vec![0, 1, 2, 3, 5, 0, 3, 2]
+        );
+        assert_eq!(
+            get_all_digits(vec![0, 123, 5032, 9]),
+            vec![0, 1, 2, 3, 5, 0, 3, 2, 9]
+        );
+    }
+}
